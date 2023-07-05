79390ad1c7b2dd00d7af6514c5306bc56847edc3
V. Jolie <vjolie03work@gmail.com>
2023-07-05 12:31:54 +0200

    Finished hero section + responsive, + briller & open sans fonts (reg & bold), + particle effect in bg

--- html/resources/views/welcome.blade.php
+++ html/resources/views/welcome.blade.php
@@ -11,31 +11,39 @@
 <body>
     <!-- navbar -->
     <!-- nav bar end-->
-    <div class="hero">
-        <h1>Comienza una nueva experiencia</h1>
-        <div>
-            <a href="#">Únete a Discord</a>
-            <a href="#">Consigue tu NFT</a>
+    <section class="hero">
+        <div class="hero-content">
+            <div id="particles-js"></div>
+            <img src="{{ asset('img/hombre_fondo_vr.png') }}" alt="Man" class="hidden">
+            <div>
+                <h1>Comienza una nueva experiencia</h1>
+                <div class="buttonWrapper">
+                    <a href="https://discord.com/invite/Kuc9H6Y5B4">Únete a Discord</a>
+                    <a href="#">Consigue tu NFT</a>
+                </div>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/company/alveariumvr" class="socials" target="_blank">
+                    <i class="fa-brands fa-linkedin-in" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://twitter.com/alvearium__" target="_blank" class="socials">
+                    <i class="fa-brands fa-twitter" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://discord.com/invite/Kuc9H6Y5B4" target="_blank" class="socials">
+                    <i class="fa-brands fa-discord" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://www.instagram.com/alvearium_/" target="_blank" class="socials">
+                    <i class="fa-brands fa-instagram" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://www.facebook.com/people/Alvearium/100083367819508/" target="_blank" class="socials">
+                    <i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i>
+                </a>
+            </div>
         </div>
-        <div>
-            <a href="#" class="socials">
-                <i class="fa-brands fa-linkedin-in" style="color: #ffffff;"></i>
-            </a>
-            <a href="#" class="socials">
-                <i class="fa-brands fa-twitter" style="color: #ffffff;"></i>
-            </a>
-            <a href="#" class="socials">
-                <i class="fa-brands fa-discord" style="color: #ffffff;"></i>
-            </a>
-            <a href="#" class="socials">
-                <i class="fa-brands fa-instagram" style="color: #ffffff;"></i>
-            </a>
-            <a href="#" class="socials">
-                <i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i>
-            </a>
-        </div>
-    </div>
+    </section>
     <script src="https://kit.fontawesome.com/3342157087.js" crossorigin="anonymous"></script>
+    <script src="lib/particles/particles.min.js"></script>
+    <script src="lib/particles/app.js"></script>
 </body>
 
 </html>
