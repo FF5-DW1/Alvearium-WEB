eaae2576a75927f0d5c97d07366f950012703681
V. Jolie <vjolie03work@gmail.com>
2023-07-06 12:23:54 +0200

    Divided css, + team img, + push in img + cards.js + merged nav, hero, about us, team

--- html/resources/views/index.blade.php
+++ html/resources/views/index.blade.php
@@ -7,9 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Alvearium</title>
 
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
-   
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/hero.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/aboutUs.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/team.css') }}">
 </head>
 
 <body>
@@ -17,7 +18,7 @@
     <!-- navbar -->
 
     <header class="main-header">
-    
+
 
         <a href="#">
             <img class="main-logo" src="img/alvearium-logo.png" alt="Logo">
@@ -30,17 +31,17 @@
                 <a class="link-item" href="#">Roadmap</a>
             </div>
         </nav>
-        
+
         <button id="button-menu" class="button-menu">
             <span></span>
             <span></span>
             <span></span>
         </button>
-        
+
     </header>
     <!-- nav bar end-->
 
-    {{-- Hero --}}
+    <!-- Hero -->
     <section class="hero">
         <div class="hero-content">
             <div id="particles-js"></div>
@@ -72,10 +73,10 @@
         </div>
     </section>
 
-    {{-- Hero end --}}
+    <!-- Hero end -->
 
-    {{-- Section Us --}}
-    
+    <!-- Section Us -->
+
     <section class="aboutUsContainer">
         <div class="aboutUsTitulo">
             <h2>Conoce más sobre Alvearium</h2>
@@ -86,8 +87,7 @@
                 <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/usOFzrD8a2E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="aboutUsPerfil">
-                
-                
+
                 <div class="aboutInstitutions">
                     <p>
                         Instituciones Públicas
@@ -124,20 +124,86 @@
                     </p>
                     <img class="aboutImgUsers" src="{{asset('img/usuarios.png')}}" alt="">
                 </div>
-                
-                
+
+
             </div>
         </div>
     </section>
 
+    <!-- Team -->
+    <section class="team-direction_slider">
+        <h2 class="tittle-section">Equipo <span class="tittle-section-second_color">Dirección</span></h2>
+        <div class="contenedor">
+            <div class="slider">
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-alejandro-1.png" alt="Imagen de perfil equipo de dirección Alejandro Ansodi">
+                    <div>
+                        <h3 class="name_equip">Alejandro Ansodi</h3>
+                        <h3 class="profile_equip">CDO (Design)</h3>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-Diana.png" alt="Imagen de perfil equipo de dirección Diana Baldero">
+                    <div>
+                        <h3 class="name_equip">Diana Baldero</h3>
+                        <h3 class="profile_equip"> CFO </h3>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-pol-1.png" alt="Imagen de perfil equipo de dirección Pol Echarri">
+                    <div>
+                        <h3 class="name_equip">Pol Echarri</h3>
+                        <h3 class="profile_equip"> CTO (Talent) </h3>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-luis-manuel-1.png" alt="Imagen de perfil equipo de dirección Luis Manuel Fernández">
+                    <div>
+                        <h3 class="name_equip">Luis Manuel Fernández</h3>
+                        <h3 class="profile_equip"> CEO</h3>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-jose-miguel-1.png" alt="Imagen de perfil equipo de dirección José Miguel Fernández">
+                    <div>
+                        <h3 class="name_equip">José Miguel Fernández</h3>
+                        <h3 class="profile_equip">CTO (Tech)</h3>
+                    </div>
+
+
+                </div>
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-luis-1.png" alt="Imagen de perfil equipo de dirección Luis Martín">
+                    <div>
+                        <h3 class="name_equip">Luis Martín</h3>
+                        <h3 class="profile_equip">CMO (Marketing)</h3>
+                    </div>
+
+                </div>
+                <div class="item">
+                    <img src="img/profile_pictures_direction/alvearium-team-oscar-1.png" alt="Imagen de perfil equipo de dirección Oscar Escallada">
+                    <div>
+                        <h3 class="name_equip">Oscar Escallada</h3>
+                        <h3 class="profile_equip"> CXO (Experience)</h3>
+                    </div>
+
+                </div>
+                <button id="next"> > </button>
+                <button id="prev">
+                    < </button>
+            </div>
+        </div>
+    </section>
 
     <script src="{{ asset('navbar.js') }}"></script>
-
     <script src="https://kit.fontawesome.com/3342157087.js" crossorigin="anonymous"></script>
-
     <script src="lib/particles/particles.min.js"></script>
     <script src="lib/particles/app.js"></script>
-    
+    <script src="js/cards.js"></script>
 </body>
 
 </html>
