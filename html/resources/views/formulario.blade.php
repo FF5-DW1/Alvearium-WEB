9617fd2e508507fd55778a00e68a14dcfa079276
fabiolatomillo <alanis.fabiola.03@gmail.com>
2023-07-14 13:01:30 +0200

    terminado

--- html/resources/views/formulario.blade.php
+++ html/resources/views/formulario.blade.php
@@ -12,27 +12,29 @@
 </head>
 
 
-
 <body>
-<form action="" class="container">
-  <div class="input-container">
-      <div class="input-content">
-          <div class="input-dist">
-              <div class="input-type">
-                   
+<div>
+    <H1>¿Quieres estar al dia de todas las novedades, o colaborar en el proyecto?Contacta con nosotros y síguenos en redes</H1>
+</div>
+    <form action="" class="container">
+        <div class="input-container">
+            <div class="input-content">
+                <div class="input-dist">
+                    <div class="input-type">
                         <input placeholder="Nombre" required="" type="text" class="input-is">  
                         <input placeholder="Teléfono" required="" type="" class="input-is">
                         <input placeholder="E-mail" required="" type="text" class="input-is">
                         <textarea placeholder="Texto" required="" type="" class="input-is"></textarea>
-              </div>
-          </div>
-      </div>
-  </div>
-        <button> 
-            Button
-        </button>
-</form>
-
+                    </div>
+                   
+                </div>
+                
+            </div>
+            <div class="button-container">      
+            <button>Enviar</button>
+            </div>
+        </div>        
+      </form>
+    </div>
 </body>
-
 </html>
