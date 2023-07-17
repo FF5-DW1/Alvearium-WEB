55a7a921cfbbe7364001c1355b9573b501730279
Bangarrett <126791771+Bangarrett@users.noreply.github.com>
2023-07-17 11:00:53 +0200

    fix roadmap

--- html/resources/views/index.blade.php
+++ html/resources/views/index.blade.php
@@ -397,67 +397,71 @@
     <section class="roadmap_container">
 
 
-        <h2 class="road_title">Road Map</h2>
+        <h2 class="road_title">Roadmap</h2>
 
 
         <div class="road_container">
             
-            <ul class="road_phase">
-                <div class="road_img_phase">
-                    <img class="road_img_phase_img" src="/img/roadmap/roadmap_fondo.png" alt="Fase 1">
-                    <p class="road_p fase1">F.I</p>
-                    <h3>
-                        2022 2023
-                    </h3>
+                <div class="phase_container">
+                    <ul class="road_phase">
+                        <div class="road_img_phase">
+                            <img class="road_img_phase_img" src="/img/roadmap/roadmap_fondo.png" alt="Fase 1">
+                            <p class="road_p fase1">F.I</p>
+                            <h3>
+                                2022 2023
+                            </h3>
+                        </div>
+                    </ul>
+                    
+                    <ul class="road_phase">
+                        <li class="road_img_phase">
+                            <img class="road_img_phase_img" src="/img/roadmap/roadmap_fondo.png" alt="Fase 2">
+                            <p class="road_p fase2">F.II</p>
+                            <h3>
+                                2022 2023
+                            </h3>
+                        </li>
+                    </ul>
+                    <ul class="road_phase">
+                            <li class="road_img_phase">
+                                <img class="road_img_phase_img" src="/img/roadmap/roadmap_fondo.png" alt="Fase 3">
+                                <p class="road_p">F.III</p>
+                                <h3>
+                                    2022 2023
+                                </h3>
+                            </li>
+                    </ul>
                 </div>
 
-            </ul>
-            <ul class="road_state">
-                <li class="road_img_state">
-                    <img class="road_img_state_img" src="/img/roadmap/road_paisaje_luz.png" alt="Paisaje de la Luz">
-                    <h3>
-                        Paisaje de la Luz
-                    </h3>
-                </li>
-            </ul>
-
-            <ul class="road_phase">
-                <li class="road_img_phase">
-                    <img class="road_img_phase_img" src="/img/roadmap/roadmap_fondo.png" alt="Fase 1">
-                    <p class="road_p fase2">F.II</p>
-                    <h3>
-                        2022 2023
-                    </h3>
-                </li>
-
-            </ul>
-            <ul class="road_state">
-                <li class="road_img_state">
-                    <img class="road_img_state_img" src="/img/roadmap/road_comercio.png" alt="Paisaje de la Luz">
-                    <h3>
-                        Madrid (Comercio, Ocio y Turismo)
-                    </h3>
-                </li>
-            </ul>
-
-            <ul class="road_phase">
-                <li class="road_img_phase">
-                    <img class="road_img_phase_img" src="/img/roadmap/roadmap_fondo.png" alt="Fase 1">
-                    <p class="road_p">F.III</p>
-                    <h3>
-                        2022 2023
-                    </h3>
-                </li>
-
-            </ul>
-            <ul class="road_state">
-                <li class="road_img_state">
-                    <img class="road_img_state_img" src="/img/roadmap/escala_Co-Desarrollo_ciudad.png" alt="Paisaje de la Luz">
-                    <h3>
-                        Escala Co-Desarrollo ciudades TOP
-                    </h3>
-                </li>
-            </ul>
+                <div class="state_container">
+                    <ul class="road_state">
+                        <li class="road_img_state">
+                            <img class="road_img_state_img" src="/img/roadmap/road_paisaje_luz.png" alt="Paisaje de la Luz">
+                            <h3>
+                                Paisaje de la Luz
+                            </h3>
+                        </li>
+                    </ul>
+                    
+                                </ul>
+                                <ul class="road_state">
+                    <li class="road_img_state">
+                        <img class="road_img_state_img" src="/img/roadmap/road_comercio.png" alt="Paisaje de la Luz">
+                        <h3>
+                            Madrid (Comercio, Ocio y Turismo)
+                        </h3>
+                    </li>
+                                </ul>
+                    
+                                <ul class="road_state">
+                    <li class="road_img_state">
+                        <img class="road_img_state_img" src="/img/roadmap/escala_Co-Desarrollo_ciudad.png" alt="Paisaje de la Luz">
+                        <h3>
+                            Escala Co-Desarrollo ciudades TOP
+                        </h3>
+                    </li>
+                                </ul>
+                </div>
 
         </div>
     </section>
