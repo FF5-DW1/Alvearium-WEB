4a8f10bb688b208b4d00eb3a436ca22d07f4fa51
Bangarrett <126791771+Bangarrett@users.noreply.github.com>
2023-07-17 13:06:32 +0200

    fix footer

--- html/resources/views/index.blade.php
+++ html/resources/views/index.blade.php
@@ -598,22 +598,31 @@
                 </div>
             </div>
         </div>
-        <div>
-            <a href="https://www.linkedin.com/company/alveariumvr" class="socials" target="_blank">
-                <i class="fa-brands fa-linkedin-in" style="color: #ffffff;"></i>
-            </a>
-            <a href="https://twitter.com/alvearium__" target="_blank" class="socials">
-                <i class="fa-brands fa-twitter" style="color: #ffffff;"></i>
-            </a>
-            <a href="https://discord.com/invite/Kuc9H6Y5B4" target="_blank" class="socials">
-                <i class="fa-brands fa-discord" style="color: #ffffff;"></i>
-            </a>
-            <a href="https://www.instagram.com/alvearium_/" target="_blank" class="socials">
-                <i class="fa-brands fa-instagram" style="color: #ffffff;"></i>
-            </a>
-            <a href="https://www.facebook.com/people/Alvearium/100083367819508/" target="_blank" class="socials">
-                <i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i>
-            </a>
+        <div class="footer_final">
+            <div class="footer_social">
+                <a href="https://www.linkedin.com/company/alveariumvr" class="socials" target="_blank">
+                    <i class="fa-brands fa-linkedin-in" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://twitter.com/alvearium__" target="_blank" class="socials">
+                    <i class="fa-brands fa-twitter" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://discord.com/invite/Kuc9H6Y5B4" target="_blank" class="socials">
+                    <i class="fa-brands fa-discord" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://www.instagram.com/alvearium_/" target="_blank" class="socials">
+                    <i class="fa-brands fa-instagram" style="color: #ffffff;"></i>
+                </a>
+                <a href="https://www.facebook.com/people/Alvearium/100083367819508/" target="_blank" class="socials">
+                    <i class="fa-brands fa-facebook-f" style="color: #ffffff;"></i>
+                </a>
+            </div>
+            <div class="footer_privacidad">
+                <p><a href="#">Conoce nuestra política de privacidad</a></p>
+                <p><a href="#">Conoce nuestra receta de cookies 😊</a></p>
+            </div>
+            <div class="footer_derechos">
+                Alvearium 2023 ®
+            </div>
         </div>
     </footer>
 
